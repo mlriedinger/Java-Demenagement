a25ac52c1847ac9288532f82ad354338ac58df25
mlriedinger <marie-laure.riedinger@le-campus-numerique.fr>
2020-05-18 15:45:46 +0200

    Amélioration du Scanner pour interagir avec le programme

--- premiersProgsProj/src/demenagementPack/TestDem.java
+++ premiersProgsProj/src/demenagementPack/TestDem.java
@@ -8,13 +8,19 @@ public class TestDem {
 	
 		System.out.println("Bienvenue");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Quelle est la capacité du camion ?");
+		
+		System.out.printf("Quelle est l'entreprise que vous avez choisi ? ");
+		String inputCompany = sc.nextLine();
+		
+		System.out.printf("Quelle est la capacité du camion ? ");
 		int inputCapacity = sc.nextInt();
 		
-		System.out.println("Combien de cartons souhaitez-vous déménager ?");
+		System.out.print("Combien de cartons souhaitez-vous déménager ? ");
 		int inputBoxes = sc.nextInt();
+		sc.close();
 		
-		Demenagement dem = new Demenagement("Entreprise Bretonne", inputCapacity);
+		
+		Demenagement dem = new Demenagement(inputCompany, inputCapacity);
 		dem.auTravail(inputBoxes);
 	}
 
