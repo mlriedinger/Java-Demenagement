e5bfaf67b25d3899d3866818e098f656e161372b
mlriedinger <marie-laure.riedinger@le-campus-numerique.fr>
2020-05-18 14:25:45 +0200

    Ajout d'un objet Scanner pour saisir directement les paramètres dans la console

--- premiersProgsProj/src/demenagementPack/TestDem.java
+++ premiersProgsProj/src/demenagementPack/TestDem.java
@@ -1,12 +1,21 @@
 package demenagementPack;
 
+import java.util.Scanner;
+
 public class TestDem {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Bienvenue");
-		Demenagement dem = new Demenagement("Entreprise Bretonne", 9);
-		dem.auTravail(34);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quelle est la capacité du camion ?");
+		int inputCapacity = sc.nextInt();
+		
+		System.out.println("Combien de cartons souhaitez-vous déménager ?");
+		int inputBoxes = sc.nextInt();
+		
+		Demenagement dem = new Demenagement("Entreprise Bretonne", inputCapacity);
+		dem.auTravail(inputBoxes);
 	}
 
 }
